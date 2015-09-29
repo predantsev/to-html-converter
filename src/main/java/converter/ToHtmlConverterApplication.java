89dd792fa2809947edb34863055d9075d5d15313
predantsev <predantsev@gmail.com>
2015-09-29 13:57:36 +0300

    Spring Boot Init

---
+++ src/main/java/converter/ToHtmlConverterApplication.java
@@ -0,0 +1,12 @@
+package converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToHtmlConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToHtmlConverterApplication.class, args);
+    }
+}
